606e4a20fdea0e647b2b165d8fe681472a6faf98
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-28 18:36:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -41,13 +41,6 @@ end
 
 
 
-
-  describe "#dennis_ritchies_language" do 
-    it "operates on the programmer_hash and returns the value of Dennis Ritchie's language as a string" do 
-      expect(dennis_ritchies_language).to eq("C")
-    end
-  end
-
   describe "#adding_matz" do 
     it "operates on the programmer_hash and adds a key/value pair to the top level of the hash, returning the newly-added-to hash" do 
       expect(adding_matz.keys).to include(:yukihiro_matsumoto)
@@ -56,7 +49,7 @@ end
 
 
 
-# # Return value of Alan Kay's :known_for key
+# # Return value of Dennis Ritchie's :languages as string
 def dennis_ritchies_language
 	programmer_hash = 
  		{
